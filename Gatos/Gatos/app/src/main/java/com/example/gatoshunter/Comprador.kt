7a47e4209e35e37fed1cb043470b5c18694b2ef2
lyninn <130926921+lyninn@users.noreply.github.com>
2025-05-21 17:02:01 +0100

    ,

--- Gatos/Gatos/app/src/main/java/com/example/gatoshunter/Comprador.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gatoshunter
-
-data class Comprador(
-    var id: Int,
-    var nombre: String,
-    var localidad: String,
-)
--- Gatos/Gatos/app/src/main/java/com/example/gatoshunter/Gato.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gatoshunter
-
-data class Gato(
-    var id: Int,
-    var nombre: String,
-    var peso: Double,
-    var localidad: String,
-    var descripcion: String
-)
